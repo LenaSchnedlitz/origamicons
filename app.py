083ac4c6950e4c12b5fe8544acc9baf28f072c2b
Mailea <lena.schnedlitz@gmx.at>
2017-03-10 22:04:44 +0100

    refactor

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from hashlib import sha1
-
-username = "mailea"
-
-sha = sha1()
-sha.update(str.encode(username))
-
-length = 160  # sha1 length * bits per hex number = 40 * 4
-
-code = str(bin(int(sha.hexdigest(), 16))[2:].zfill(length))
-code = [code[i:i + 4] for i in range(0, length - 2)]
+def hex_to_bin(sha_hash):
+    length = 160  # hash length * bits per hex number
+    bin_string = str(bin(int(sha_hash, 16))[2:].zfill(length))
+    return [bin_string[i:i + 4] for i in range(0, length - 2)]
 
 
-def generator():
+def sha1_hash(username):
+    from hashlib import sha1
+    sha = sha1()
+    sha.update(str.encode(username))
+    return hex_to_bin(sha.hexdigest())
+
+
+def split_position():
     distances = [2] * 22 + [3] * 38
     i = 0
     while distances:
@@ -19,6 +19,13 @@ def generator():
         i += distances.pop()
 
 
-gen = generator()
-formatted_code_parts = [code[i] for i in gen]
-print(formatted_code_parts)
+def split_for_drawing(bin_hash):
+    indices = split_position()
+    return [bin_hash[i] for i in indices]
+
+
+name_hash = sha1_hash("mailea")
+hash_parts = split_for_drawing(name_hash)
+
+
+print(hash_parts)
