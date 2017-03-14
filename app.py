b545868f8412de8ec0937e744b128732dec21fbf
Mailea <lena.schnedlitz@gmx.at>
2017-03-14 19:12:56 +0100

    rewrite split algorithm

--- app.py
+++ app.py
@@ -1,35 +1,70 @@
-def hex_to_bin(sha_hash):
-    """Convert SHA-1 hash to binary and return result as a string."""
-    length = 160  # hash length * bits per hex number
-    return str(bin(int(sha_hash, 16))[2:].zfill(length))
+DOTS = 61  # number of drawn 'dots' in image
 
 
 def sha1_hash(username):
-    """Return (hexadecimal) SHA-1 hash of passed username."""
+    """Return SHA-1 hash of passed username as string of zeros and ones."""
+
+    def hex_to_bin(sha_hash):
+        """Convert SHA-1 (hex) hash to binary number string."""
+        length = 160  # hash length * bits per hex number
+        return str(bin(int(sha_hash, 16))[2:].zfill(length))
+
     from hashlib import sha1
     sha = sha1()
     sha.update(str.encode(username))
     return hex_to_bin(sha.hexdigest())
 
 
-def split_position():
-    """Arbitrary algorithm"""
-    distances = [2] * 22 + [3] * 38
-    i = 0
-    while distances:
-        yield i
-        i += distances.pop()
+def split(string, position):
+    """Split string at given position, return both parts."""
+    return string[:position], string[position:]
 
 
-def split_for_drawing(bin_hash):
-    """Split binary (str) hash into 61 parts of length 4."""
-    parts = [bin_hash[i:i + 4] for i in range(0, len(bin_hash) - 2)]
-    indices = split_position()
-    return [parts[i] for i in indices]
+def get_dot_indices(margin=8, size=250):
+    """Create tuples that 'draw' a circle."""
+    shape = ((0, 0, 0, 1, 1, 1, 0, 0, 0),
+             (0, 1, 1, 1, 1, 1, 1, 1, 0),
+             (0, 1, 1, 1, 1, 1, 1, 1, 0),
+             (1, 1, 1, 1, 1, 1, 1, 1, 1),
+             (1, 1, 1, 1, 1, 1, 1, 1, 1),
+             (1, 1, 1, 1, 1, 1, 1, 1, 1),
+             (0, 1, 1, 1, 1, 1, 1, 1, 0),
+             (0, 1, 1, 1, 1, 1, 1, 1, 0),
+             (0, 0, 0, 1, 1, 1, 0, 0, 0))
+
+    distance = (size - 2 * margin) // len(shape)
+    coords = [margin + distance // 2 + i * distance for i in range(len(shape))]
+
+    tuples = []
+    for i, row in enumerate(shape):
+        for j, cell in enumerate(row):
+            if cell:
+                tuples.append((coords[i], coords[j]))
+
+    return tuples
+
+
+def draw_dots(hash_parts):
+    from PIL import Image, ImageDraw
+    img = Image.new('RGBA', (250, 250), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    r = 9
+    tuples = get_dot_indices()
+    for i in range(len(tuples)):
+        x, y = tuples[i]
+        draw.ellipse((x - r, y - r, x + r, y + r),
+                     fill=(255, 0, 0, 0))
 
 
 name_hash = sha1_hash("mailea")
-hash_parts = split_for_drawing(name_hash)
+img_hash = name_hash * 2
 
+dot_data, img_hash = split(img_hash, 5 * DOTS)
+dot_data = [dot_data[i:i + 5] for i in range(0, len(dot_data), 5)]
 
-print(hash_parts)
+palette, img_hash = split(img_hash, 5)
+bg, img_hash = split(img_hash, 2)
+shape, img_hash = split(img_hash, 2)
+
+draw_dots(dot_data)
+
