6f3b3d868486d7bb2377ea3b73d6ac113c5d7a3c
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-02-17 20:15:39 +0100

    Replace some quotation marks

--- origamicon/app.py
+++ origamicon/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("landing.html")
+    return render_template('landing.html')
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=['POST'])
 def update_avatar():
-    text = request.form["text"]
+    text = request.form['text']
     if not text:
-        return render_template("landing.html")
+        return render_template('landing.html')
 
     import io as io
     import base64 as b
@@ -20,13 +20,13 @@ def update_avatar():
 
     buffer = io.BytesIO()
     image = gen.create_origamicon(text)
-    image.save(buffer, format="PNG")
+    image.save(buffer, format='PNG')
     image_data = buffer.getvalue()
     image_data = b.b64encode(image_data)
     image_data = image_data.decode()
 
-    return render_template("main.html", name=text, image=image_data)
+    return render_template('main.html', name=text, image=image_data)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
