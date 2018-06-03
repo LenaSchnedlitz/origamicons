f87d2c2c97319cd4faa97f37f2c0cb989f47e61d
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-03 12:47:27 +0200

    Improve routing and image handling

--- origamicon/app.py
+++ origamicon/app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, request
 
+import io
+import base64 as b64
+
+import generator
+
 app = Flask(__name__)
+
+
+def page(**kwargs):
+    return render_template('main.html', **kwargs)
 
 
 @app.route("/")
 def home():
-    return render_template('main.html')
+    return page()
 
 
 @app.route("/", methods=['POST'])
 def update_avatar():
-    text = request.form['text']
+    text = request.form['origamicon-text']
     if not text:
-        return render_template('main.html')
-
-    import io as io
-    import base64 as b
-    import generator as gen
+        return page()
 
     buffer = io.BytesIO()
-    image = gen.create_origamicon(text)
+    image = generator.create_origamicon(text)
     image.save(buffer, format='PNG')
     image_data = buffer.getvalue()
-    image_data = b.b64encode(image_data)
-    image_data = image_data.decode()
+    image_data = b64.b64encode(image_data).decode()
 
-    return render_template('main.html', name=text, image=image_data)
+    return page(name=text, image='data:;base64, {}'.format(image_data))
 
 
 if __name__ == '__main__':
