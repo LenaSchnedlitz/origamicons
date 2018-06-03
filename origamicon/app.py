4ced7dfab5b51cb78849bc9bab9c9a8377dc1700
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-03 14:01:05 +0200

    Add option to access origamicons directly

--- origamicon/app.py
+++ origamicon/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 import io
 import base64 as b64
@@ -18,18 +18,29 @@ def home():
 
 
 @app.route("/", methods=['POST'])
-def update_avatar():
+def update_origamicon():
     text = request.form['origamicon-text']
     if not text:
         return page()
 
     buffer = io.BytesIO()
-    image = generator.create_origamicon(text)
-    image.save(buffer, format='PNG')
+    origamicon = generator.create_origamicon(text)
+    origamicon.save(buffer, format='PNG')
     image_data = buffer.getvalue()
     image_data = b64.b64encode(image_data).decode()
+    image = 'data:;base64, {}'.format(image_data)
 
-    return page(name=text, image='data:;base64, {}'.format(image_data))
+    return page(name=text, image=image)
+
+
+@app.route("/<text>", methods=['GET'])
+def load_origamicon(text):
+    buffer = io.BytesIO()
+    origamicon = generator.create_origamicon(text)
+    origamicon.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype='image/png')
 
 
 if __name__ == '__main__':
